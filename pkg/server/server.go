03eb60ad16ad2fe0023ad4a39f9b032f5bdf1f97
Yogeshpant992 <pantyogesh992@gmail.com>
2026-03-04 18:40:37 +0530

    Adding Server file

---
+++ pkg/server/server.go
@@ -0,0 +1,44 @@
+package server
+
+import "net/http"
+// Server is an HTTP Server 
+type Server struct{
+
+}
+
+//New is a new server 
+func New() *Server{
+	return &Server{}
+}
+
+var indexPage = `
+<!DOCTYPE html>
+<html>
+    <body> 
+	<h1> My first heading </h1>
+	<p> My first paragraph </p>
+	</body>
+</html>`
+
+var userInfo = `{
+	"name": "testing",
+	"age":21
+}`
+
+
+//HandleIndex handle the index path "/"
+func  ( s *Server) HandleIndex(w  http.ResponseWriter, r *http.Request) {
+	w.Header().Add("content-type", "text/html")
+		w.WriteHeader(http.StatusOK) // send the request which it is either requested or stuats ok and all basically provide you the status of the code.
+		w.Write([]byte(indexPage))
+
+} 
+
+
+// HandleIndex handle the user path "/users"
+func  ( s *Server) HandleUsers(w  http.ResponseWriter, r *http.Request) {
+	w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusOK) // send the request which it is either requested or stuats ok and all basically provide you the status of the code.
+		w.Write([]byte(userInfo))
+
+} 
